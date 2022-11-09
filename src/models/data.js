e9ccb01d03ce80f09810011f643dd79e8c4fff35
Adrian Wysocki <a.wysocki@uniqsoft.pl>
2022-11-09 20:01:51 +0100

    Add db models

---
+++ src/models/data.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const dataSchema = new mongoose.Schema({
+  ml: {
+    type: Number,
+    required: true,
+  },
+  pktSkp: {
+    type: String,
+  },
+  dost: {
+    type: String,
+  },
+  surname: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  fat: {
+    type: String,
+  },
+  protein: {
+    type: String,
+  },
+  lactose: {
+    type: String,
+  },
+  smb: {
+    type: String,
+  },
+  lks: {
+    type: Number,
+    required: true,
+  },
+  old: {
+    type: Number,
+    required: true,
+  },
+  pzam: {
+    type: String,
+  },
+  water: {
+    type: String,
+  },
+  sh: {
+    type: String,
+  },
+  pzam: {
+    type: String,
+  },
+  urea: {
+    type: String,
+  },
+  code: {
+    type: String,
+  },
+  route: {
+    type: String,
+  },
+  course: {
+    type: String,
+  },
+  datapob: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Data", dataSchema);
