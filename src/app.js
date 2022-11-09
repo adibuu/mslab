7330a27f6cf714bb3fd3140816fedc02dc26bd92
Adrian Wysocki <a.wysocki@uniqsoft.pl>
2022-11-09 19:23:03 +0100

    Connect to db

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+require("./db/db-connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(helmet());
+app.use(cors());
 
 app.listen(PORT, () => console.log("Server is running on " + PORT));
