82f5f7dd05667d880e1cf4b08ce7839736521135
Adrian Wysocki <a.wysocki@uniqsoft.pl>
2022-11-13 20:47:34 +0100

    Preapre hook form for data average

--- src/adminjs/components/DataAverage.jsx
+++ src/adminjs/components/DataAverage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import {
   Box,
   Button,
@@ -8,8 +8,10 @@ import {
   Header,
 } from "@adminjs/design-system";
 import { ApiClient } from "adminjs";
-import axios from "axios";
 import Select from "react-select";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import pl from "date-fns/locale/pl";
 
 const api = new ApiClient();
 
@@ -17,6 +19,28 @@ const DataAverage = () => {
   const [loading, setLoading] = useState(true);
   const [ml, setMl] = useState();
   const [surnames, setSurnames] = useState();
+  const mlOptions = useMemo(
+    () => ml?.map((ml) => ({ value: ml, label: ml })),
+    [ml]
+  );
+  const surnamesOptions = useMemo(
+    () =>
+      surnames?.map((surname) => ({
+        value: surname,
+        label: surname,
+      })),
+    [surnames]
+  );
+  const {
+    control: controlLKS,
+    handleSubmit: handleSubmitLKS,
+    formState: { errors: errorsLKS, isSubmitting: isSubmittingLKS },
+  } = useForm();
+  const {
+    control: controlOLD,
+    handleSubmit: handleSubmitOLD,
+    formState: { errors: errorsOLD, isSubmitting: isSubmittingOLD },
+  } = useForm();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -47,87 +71,169 @@ const DataAverage = () => {
     fetchData();
   }, []);
 
+  const onSubmitLKS = async (data) => console.log(data);
+
+  const onSubmitOLD = async (data) => console.log(data);
+
   return (
     <Box variant="grey">
       {loading ? (
         <Loader />
       ) : (
         <>
-          <Box variant="card">
-            <Header>Oblicz wartość LKS</Header>
-            <Text fontSize={"lg"} marginBottom="5px">
-              Data
-            </Text>
-            <DatePicker />
-            <Text marginTop={"20px"} fontSize={"lg"} marginBottom="5px">
-              Numer ML
-            </Text>
-            <Select
-              options={ml?.map((ml) => ({ value: ml, label: ml }))}
-              placeholder="Wybierz..."
-              isClearable
-              noOptionsMessage={({ inputValue }) =>
-                !inputValue ? noOptionsText : "Brak opcji"
-              }
-            />
-            {surnames?.[0] && (
-              <>
-                <Text marginTop={"20px"} marginBottom="5px" fontSize={"lg"}>
-                  Nazwisko
+          <form onSubmit={handleSubmitLKS(onSubmitLKS)}>
+            <Box variant="card">
+              <Header>Oblicz wartość LKS</Header>
+              <Text fontSize={"lg"} marginBottom="5px">
+                Data
+              </Text>
+              <Controller
+                control={controlLKS}
+                name="dateLKS"
+                rules={{ required: true }}
+                render={({ field: { onChange, value, ref } }) => (
+                  <DatePicker
+                    onChange={onChange}
+                    value={value}
+                    locale={pl}
+                    ref={ref}
+                    propertyType="date"
+                  />
+                )}
+              />
+              {errorsLKS.dateLKS && (
+                <Text mt={3} color="red">
+                  Data jest wymagana
                 </Text>
-                <Select
-                  options={surnames?.map((surname) => ({
-                    value: surname,
-                    label: surname,
-                  }))}
-                  placeholder="Wybierz..."
-                  isClearable
-                  noOptionsMessage={({ inputValue }) =>
-                    !inputValue ? noOptionsText : "Brak opcji"
-                  }
-                />
-              </>
-            )}
-            <Button marginTop={"20px"}>Oblicz</Button>
-          </Box>
+              )}
+              <Text marginTop={"20px"} fontSize={"lg"} marginBottom="5px">
+                Numer ML
+              </Text>
+              <Controller
+                control={controlLKS}
+                name="mlLKS"
+                render={({ field: { onChange, value, ref } }) => (
+                  <Select
+                    onChange={(selectedValue) => onChange(selectedValue.value)}
+                    value={mlOptions.find((c) => value === c.value)}
+                    ref={ref}
+                    options={mlOptions}
+                    placeholder="Wybierz..."
+                    isClearable
+                    noOptionsMessage={({ inputValue }) =>
+                      !inputValue ? noOptionsText : "Brak opcji"
+                    }
+                  />
+                )}
+              />
+              {surnames?.[0] && (
+                <>
+                  <Text marginTop={"20px"} marginBottom="5px" fontSize={"lg"}>
+                    Nazwisko
+                  </Text>
+                  <Controller
+                    control={controlLKS}
+                    name="surnameLKS"
+                    render={({ field: { onChange, value, ref } }) => (
+                      <Select
+                        onChange={(selectedValue) =>
+                          onChange(selectedValue.value)
+                        }
+                        value={surnamesOptions.find((c) => value === c.value)}
+                        ref={ref}
+                        options={surnamesOptions}
+                        placeholder="Wybierz..."
+                        isClearable
+                        noOptionsMessage={({ inputValue }) =>
+                          !inputValue ? noOptionsText : "Brak opcji"
+                        }
+                      />
+                    )}
+                  />
+                </>
+              )}
+              <Button marginTop={"20px"} type="submit">
+                Oblicz
+              </Button>
+            </Box>
+          </form>
 
-          <Box variant="card" marginTop={"50px"}>
-            <Header>Oblicz wartość OLD</Header>
-            <Text fontSize={"lg"} marginBottom="5px">
-              Data
-            </Text>
-            <DatePicker />
-            <Text marginTop={"20px"} fontSize={"lg"} marginBottom="5px">
-              Numer ML
-            </Text>
-            <Select
-              options={ml?.map((ml) => ({ value: ml, label: ml }))}
-              placeholder="Wybierz..."
-              isClearable
-              noOptionsMessage={({ inputValue }) =>
-                !inputValue ? noOptionsText : "Brak opcji"
-              }
-            />
-            {surnames?.[0] && (
-              <>
-                <Text marginTop={"20px"} marginBottom="5px" fontSize={"lg"}>
-                  Nazwisko
+          <form onSubmit={handleSubmitOLD(onSubmitOLD)}>
+            <Box variant="card" marginTop={"50px"}>
+              <Header>Oblicz wartość OLD</Header>
+              <Text fontSize={"lg"} marginBottom="5px">
+                Data
+              </Text>
+              <Controller
+                control={controlOLD}
+                name="dateOLD"
+                rules={{ required: true }}
+                render={({ field: { onChange, value, ref } }) => (
+                  <DatePicker
+                    onChange={onChange}
+                    value={value}
+                    locale={pl}
+                    ref={ref}
+                    propertyType="date"
+                  />
+                )}
+              />
+              {errorsOLD.dateOLD && (
+                <Text mt={3} color="red">
+                  Data jest wymagana
                 </Text>
-                <Select
-                  options={surnames?.map((surname) => ({
-                    value: surname,
-                    label: surname,
-                  }))}
-                  placeholder="Wybierz..."
-                  isClearable
-                  noOptionsMessage={({ inputValue }) =>
-                    !inputValue ? noOptionsText : "Brak opcji"
-                  }
-                />
-              </>
-            )}
-            <Button marginTop={"20px"}>Oblicz</Button>
-          </Box>
+              )}
+              <Text marginTop={"20px"} fontSize={"lg"} marginBottom="5px">
+                Numer ML
+              </Text>
+              <Controller
+                control={controlOLD}
+                name="mlOLD"
+                render={({ field: { onChange, value, ref } }) => (
+                  <Select
+                    onChange={(selectedValue) => onChange(selectedValue.value)}
+                    value={mlOptions.find((c) => value === c.value)}
+                    ref={ref}
+                    options={mlOptions}
+                    placeholder="Wybierz..."
+                    isClearable
+                    noOptionsMessage={({ inputValue }) =>
+                      !inputValue ? noOptionsText : "Brak opcji"
+                    }
+                  />
+                )}
+              />
+              {surnames?.[0] && (
+                <>
+                  <Text marginTop={"20px"} marginBottom="5px" fontSize={"lg"}>
+                    Nazwisko
+                  </Text>
+                  <Controller
+                    control={controlOLD}
+                    name="surnameOLD"
+                    render={({ field: { onChange, value, ref } }) => (
+                      <Select
+                        onChange={(selectedValue) =>
+                          onChange(selectedValue.value)
+                        }
+                        value={surnamesOptions.find((c) => value === c.value)}
+                        ref={ref}
+                        options={surnamesOptions}
+                        placeholder="Wybierz..."
+                        isClearable
+                        noOptionsMessage={({ inputValue }) =>
+                          !inputValue ? noOptionsText : "Brak opcji"
+                        }
+                      />
+                    )}
+                  />
+                </>
+              )}
+              <Button marginTop={"20px"} type="submit">
+                Oblicz
+              </Button>
+            </Box>
+          </form>
         </>
       )}
     </Box>
